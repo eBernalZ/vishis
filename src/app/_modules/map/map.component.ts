881705c47e3a240a153ad6909adb553eb596f196
Emilio Bernal <ebernalzubieta@gmail.com>
2023-06-18 21:07:05 -0600

    changing to Mapbox

--- src/app/_modules/map/map.component.ts
+++ src/app/_modules/map/map.component.ts
@@ -15,6 +15,9 @@ import { SimpleRenderer } from "@arcgis/core/renderers";
 import { SimpleMarkerSymbol } from '@arcgis/core/symbols';
 import { environment } from 'src/environments/environment';
 
+// Testing mapbox
+import * as mapboxgl from 'mapbox-gl';
+
 // i18n imports
 import { TranslocoService } from '@ngneat/transloco';
 
@@ -30,16 +33,24 @@ export class MapComponent implements OnInit, AfterViewInit {
   showSettings = false;
   showAuth = false;
   showDrawer = false;
-  locale = 'en';
+  locale = 'es';
   countries = ["AF", "AL", "DZ", "AS", "AD", "AO", "AI", "AQ", "AG", "AR", "AM", "AW", "AU", "AT", "AZ", "BS", "BH", "BD", "BB", "BY", "BE", "BZ", "BJ", "BM", "BT", "BO", "BQ", "BA", "BW", "BV", "BR", "IO", "BN", "BG", "BF", "BI", "CV", "KH", "CM", "CA", "KY", "CF", "TD", "CL", "CN", "CX", "CC", "CO", "KM", "CD", "CG", "CK", "CR", "HR", "CU", "CW", "CY", "CZ", "CI", "DK", "DJ", "DM", "DO", "EC", "EG", "SV", "GQ", "ER", "EE", "SZ", "ET", "FK", "FO", "FJ", "FI", "FR", "GF", "PF", "TF", "GA", "GM", "GE", "DE", "GH", "GI", "GR", "GL", "GD", "GP", "GU", "GT", "GG", "GN", "GW", "GY", "HT", "HM", "VA", "HN", "HK", "HU", "IS", "IN", "ID", "IR", "IQ", "IE", "IM", "IL", "IT", "JM", "JP", "JE", "JO", "KZ", "KE", "KI", "KP", "KR", "KW", "KG", "LA", "LV", "LB", "LS", "LR", "LY", "LI", "LT", "LU", "MO", "MG", "MW", "MY", "MV", "ML", "MT", "MH", "MQ", "MR", "MU", "YT", "MX", "FM", "MD", "MC", "MN", "ME", "MS", "MA", "MZ", "MM", "NA", "NR", "NP", "NL", "NC", "NZ", "NI", "NE", "NG", "NU", "NF", "MP", "NO", "OM", "PK", "PW", "PS", "PA", "PG", "PY", "PE", "PH", "PN", "PL", "PT", "PR", "QA", "MK", "RO", "RU", "RW", "RE", "BL", "SH", "KN", "LC", "MF", "PM", "VC", "WS", "SM", "ST", "SA", "SN", "RS", "SC", "SL", "SG", "SX", "SK", "SI", "SB", "SO", "ZA", "GS", "SS", "ES", "LK", "SD", "SR", "SJ", "SE", "CH", "SY", "TW", "TJ", "TZ", "TH", "TL", "TG", "TK", "TO", "TT", "TN", "TR", "TM", "TC", "TV", "UG", "UA", "AE", "GB", "UM", "US", "UY", "UZ", "VU", "VE", "VN", "VG", "VI", "WF", "EH", "YE", "ZM", "ZW", "AX"];
   statuses = [0, 1];
   loginloading = false;
+  eventloading = false;
   searchloading = false;
   searchParams!: UntypedFormGroup;
   loginForm!: UntypedFormGroup;
+  eventForm!: UntypedFormGroup;
   @ViewChild('map', { static: true }) private map!: ElementRef;
   @ViewChild('settings', { static: true }) private settings!: ElementRef;
   @ViewChild('searchBar', { read: ElementRef, static: false }) private searchBar!: ElementRef;
+  @ViewChild('create', { read: ElementRef, static: false }) private create!: ElementRef;
+  style = 'mapbox://styles/mapbox/streets-v11';
+  map2!: mapboxgl.Map;
+  lat = 37.75;
+  lng = -122.41;
+
 
   constructor(
     private fb: UntypedFormBuilder,
@@ -59,18 +70,40 @@ export class MapComponent implements OnInit, AfterViewInit {
       password: [null, [Validators.required]],
       remember: [true]
     });
+
+    this.eventForm = this.fb.group({
+      event_type: [null, [Validators.required]],
+      start_date: [null, [Validators.required]],
+      end_date: [null, [Validators.required]],
+      name: [null, [Validators.required]],
+      description: [null, [Validators.maxLength(100), Validators.required]],
+      event_status: [null, [Validators.required]],
+      author_id: [null, [Validators.required]],
+    });
   }
 
   ngOnInit(): void {
-
-
+    this.translocoService.setActiveLang(this.locale);
+    this.map2 = new mapboxgl.Map({
+        container: 'map',
+        // @ts-ignore
+        projection: 'globe',
+        style: this.style,
+        zoom: 2,
+        accessToken: environment.MAPBOX_API_KEY,
+        center: [this.lng, this.lat]});
+    // Add map controls
+    this.map2.addControl(new mapboxgl.NavigationControl());
+    this.map2.on('style.load', () => {
+      this.map2.setFog({});
+    });
   }
 
   ngAfterViewInit(): void {
-    this.initializeMap().then(() => {
+    // this.initializeMap().then(() => {
       // The map has been initialized
       // console.log('The map is ready.');
-    });
+    // });
   }
 
   // ARCGIS MAP VARIABLES
@@ -130,6 +163,7 @@ export class MapComponent implements OnInit, AfterViewInit {
     // Add Search widget
     view.ui.add(this.searchBar.nativeElement, "bottom-right");
     view.ui.add(this.settings.nativeElement, "top-right");
+    view.ui.add(this.create.nativeElement, "bottom-left");
     this.view = view;
 
     return this.view.when();
@@ -204,7 +238,7 @@ export class MapComponent implements OnInit, AfterViewInit {
       setTimeout(() => {
         this.loginloading = false;
         this.closeAuth();
-        this.message.success(this.translocoService.translate('auth.loginerror'));
+        this.message.success(this.translocoService.translate('auth.loginsuccess'));
       }, 2000);
     } else {
       Object.values(this.loginForm.controls).forEach(control => {
@@ -212,6 +246,25 @@ export class MapComponent implements OnInit, AfterViewInit {
           control.markAsDirty();
           control.updateValueAndValidity({ onlySelf: true });
           this.loginloading = false;
+        }
+      });
+    }
+  }
+
+  createEvent(): void {
+    this.eventloading = true;
+    if (this.eventForm.valid) {
+      console.log('submit', this.eventForm.value);
+      setTimeout(() => {
+        this.eventloading = false;
+        this.closeDrawer();
+      }, 2000);
+    } else {
+      Object.values(this.eventForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+          this.eventloading = false;
         }
       });
     }
